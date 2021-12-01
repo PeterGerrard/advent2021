7c47d44cd71e864cef3f6e1abd0b17730893ce2e
Peter Gerrard <peter.gerrard@red-gate.com>
2021-12-01 12:57:24 +0000

    Simplify solve1

--- src/day01.hs
+++ src/day01.hs
@@ -1,8 +1,7 @@
-solve1 :: [Integer] -> Integer
-solve1 [] = 0
-solve1 (x : xs) = snd $ foldl (\(z, c) y -> (y, c + (if y > z then 1 else 0))) (x, 0) xs
+solve1 :: [Integer] -> Int
+solve1 xs = length $ filter (< 0) $ zipWith (-) xs (drop 1 xs)
 
-solve2 :: [Integer] -> Integer
+solve2 :: [Integer] -> Int
 solve2 xs = solve1 $ zipWith3 (\a b -> (+) ((+) a b)) xs (drop 1 xs) (drop 2 xs)
 
 main = interact $ show . solve2 . map read . lines
