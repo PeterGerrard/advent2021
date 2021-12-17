9b8afe849fe2927b873d2dc5dc9fe04fcd9f7664
Peter Gerrard <peter.gerrard@red-gate.com>
2021-12-17 14:05:40 +0000

    Add 2st quickcheck

--- tests/Day17.tests.hs
+++ tests/Day17.tests.hs
@@ -3,14 +3,11 @@ module Main (main) where
 import qualified Day17
 import System.Exit
 import Test.HUnit
+import Test.QuickCheck
+
+prop_nondescendinginfpathX :: NonNegative Integer -> Bool
+prop_nondescendinginfpathX (NonNegative x) = notElem GT . take 1000 . (\xs -> zipWith compare xs (drop 1 xs)) $ Day17.getPathX x
 
 main :: IO ()
-main = do
-  counts2 <-
-    runTestTT
-      ( test
-          [
-              TestCase (assertEqual "Example" 2 (1+1))
-          ]
-      )
-  if errors counts2 + failures counts2 == 0 then exitSuccess else exitFailure
+main =
+  quickCheck prop_nondescendinginfpathX
